c09bcc7315f8571cdd55c52b698dcebbad83c274
Mara <42477471+maralihart@users.noreply.github.com>
2023-01-14 01:12:29 +0000

    removed all code

--- script.js
+++ script.js
@@ -1,43 +1,20 @@
 // Generate a random number between 1 and 100
-const answer = Math.floor(Math.random() * 100) + 1;
-
-// Get a reference to the form and the message element
-const form = document.querySelector("form");
-const message = document.querySelector("#message");
+// Get a reference to the form and the message element (use document.querySelector)
 
 // BONUS: Reference the previous element
-const previous = document.querySelector("#previous");
 
 // Add a submit event listener to the form
-form.addEventListener("submit", function(event) {
   // Prevent the form from being submitted
-  event.preventDefault();
 
-  // Get the user"s guess
-  let guess = Number(form.guess.value);
+  // Get the user's guess
 
   // BONUS: Create a list item element that will go into a list so user can see previous choices. Call it previousGuessEntry
-  const previousGuessEntry = document.createElement("li");
 
   // Check if the guess is too high, too low, or correct ("Try lower", "Try higher", "You got it!")
   // Change the message.textContent to the appropriate message
   // BONUS: Change the previousGuessEntry.textContent to give information about guess
 
-  if (guess > answer) {
-    message.textContent = "Try a lower number.";
-    previousGuessEntry.textContent = "It's lower than " + guess + ".";
-  } else if (guess < answer) {
-    message.textContent = "Try a higher number.";
-    previousGuessEntry.textContent = "It's higher than " + guess + ".";
-  } else {
-    message.textContent = "Congratulations, you guessed the correct number: " + answer + "!";
-    previousGuessEntry.textContent = "It was " + answer + "!";
-  }
-
   // Append previousGuessEntry to previous to add the list item under the unordered list
-  previous.append(previousGuessEntry);
-
-});
 
 // Additional practice
 // Use functions to introduce rounds
